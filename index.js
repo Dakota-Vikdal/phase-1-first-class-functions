d742a8dcb963033bbf3ff8b95808ecaddf73af00
Dakota Vikdal <dakota.james19@hotmail.com>
2023-01-11 21:52:11 -0500

    First-Class Functions

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction(callback) {
+    console.log(callback());
+}
+
+function returnsANamedFunction() {
+    return receivesAFunction
+}
+
+
+function returnsAnAnonymousFunction() {
+    return function () {
+        
+    }
+}
